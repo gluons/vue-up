4f96ce5d37fcadaec481240d6ff17da850faae2b
Saran Tanpituckpong <sarunta@gmail.com>
2018-11-19 16:52:04 +0700

    :white_check_mark: Add test for SSR

---
+++ test/HelloSSR.spec.ts
@@ -0,0 +1,28 @@
+/**
+ * @jest-environment node
+ */
+
+import { renderToString } from '@vue/server-test-utils';
+import { createLocalVue } from '@vue/test-utils';
+import cheerio from 'cheerio';
+
+import HelloPlugin from './fixture/dist/hello-plugin.ssr';
+
+const localVue = createLocalVue();
+localVue.use(HelloPlugin);
+
+const App = localVue.extend({
+	template: '<Hello/>'
+});
+
+describe('Components', () => {
+	test('should get an expect Hello component', () => {
+		const renderedString = renderToString(App, {
+			localVue
+		});
+		const $ = cheerio.load(renderedString);
+
+		expect($('#hello span').length).toBeGreaterThan(0);
+		expect($('#hello span').text()).toEqual('Hello, World!');
+	});
+});
