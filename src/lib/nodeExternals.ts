3c48ed44b5b8baa0feba85746d636893343c8b90
Saran Tanpituckpong <sarunta@gmail.com>
2018-12-23 22:40:32 +0700

    :sparkles: Support adding more external module in `nodeExternals`

--- src/lib/nodeExternals.ts
+++ src/lib/nodeExternals.ts
@@ -4,12 +4,30 @@ import validateNPMPackageName from 'validate-npm-package-name';
 const nodeModulesRegex = /[\/\\]node_modules[\/\\]/;
 
 /**
+ * Options of `nodeExternals`.
+ *
+ * @export
+ * @interface NodeExternalsOptions
+ */
+export interface NodeExternalsOptions {
+	/**
+	 * List of additional module RegEx to exclude from bundle
+	 *
+	 * @type {RegExp[]}
+	 * @memberof NodeExternalsOptions
+	 */
+	extra?: RegExp[];
+}
+
+/**
  * Create function to treat node modules as external to the bundle.
  *
  * @export
  * @returns {IsExternal}
  */
-export default function nodeExternals(): IsExternal {
+export default function nodeExternals(options: NodeExternalsOptions = {}): IsExternal {
+	const { extra } = options;
+
 	return (id: string): boolean => {
 		// Node package name
 		const validateResult = validateNPMPackageName(id);
@@ -21,6 +39,14 @@ export default function nodeExternals(): IsExternal {
 		// In `node_modules` directory
 		const isInNodeModules = nodeModulesRegex.test(id);
 		if (isInNodeModules) {
+			return true;
+		}
+
+		if (
+			Array.isArray(extra) &&
+			extra.length > 0 &&
+			extra.some(extraRegex => extraRegex.test(id))
+		) {
 			return true;
 		}
 
